47a187fc42cb62e6d39bc5ec5491e9f9317bb49c
bobbylammy71446307 <lamchunpok@gmail.com>
2025-07-14 11:37:01 +0800

    remove unessary files

--- build/cmdvel_to_ackermann/CMakeFiles/cmdvel_to_ackermann_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cmdvel_to_ackermann_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cmdvel_to_ackermann_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/cmdvel_to_ackermann/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aimo/amr_ws/src/ackermann_control/cmdvel_to_ackermann
-# Build directory: /home/aimo/amr_ws/build/cmdvel_to_ackermann
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
